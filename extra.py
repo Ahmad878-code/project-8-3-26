a4af153e0122af7a4b60566117fabfff7e58844b
Ahmad878-code <affan.grewal73@gmail.com>
2026-03-08 16:04:02 +0500

    extra

---
+++ extra.py
@@ -0,0 +1,16 @@
+class Base:
+    def __init__(self):
+        self.a = "GeeksforGeeks"
+        self._c= "GeeksforGeeks"
+    
+
+class Derived(Base):
+    def __init__(self):
+
+        Base.__init__(self)
+        print("Calling private member of base class")
+        print("self._c")
+
+obj1 = Base()
+print(obj1.a)
+
